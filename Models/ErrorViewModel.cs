fc363648dac6f9f7e0a18ae01d10c2f754245ab2
saziye <szy.dag2222@gmail.com>
2023-11-19 13:53:46 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroducingTheWondersOfTheWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
